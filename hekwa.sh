7b66a31fc95055e6b33d3dc877103db19581e4d0
Mira polos <66188045+mira-polos@users.noreply.github.com>
2020-06-18 22:22:43 +0700

    Hai gais hai guys hai gas
    Gunakan dengan bijak ini cuman main main

---
+++ hekwa.sh
@@ -0,0 +1,136 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+kuning='\033[1;33m'
+clear
+sleep 1
+echo $green "" "" "" "" "" "" "" "" "" "" "untuk menggunakan tools ini"
+echo
+echo $kuning "di korban 1 isi nomor pacar.ibu.kaka.adik atau teman yang ingin di lihat"
+echo
+echo $green "di korban 2 isi nomor lawan bicara"
+echo
+echo -n "tekan enter jika sudah mengerti ...............";read lop
+sleep 3
+clear
+termux-open-url https://youtu.be/-iZ-LKwM7yY
+#bagian editor
+echo $green "
+█████████
+█▄█████▄█
+█▼▼▼▼▼             ╔════•ೋೋ•════╗
+█                     HACK WA
+█▲▲▲▲▲             ╚════•ೋೋ•════╝
+█████████
+ ██ ██ "
+echo $green  "*************************************************************"
+echo $kuning "AOUTHOR : mira polos"
+echo $kuning "GITHUB  : https://github.com/mira polos"
+echo $kuning "YOUTUBE : CYTOM "
+echo $green  "*************************************************************"
+echo
+sleep 1
+echo -n $cyan   "masukan nomor korban 1 : ";read lol
+echo
+echo -n $green  "masukan nomor korban 2 : ";read iuy
+echo
+sleep 2
+clear
+echo $red    "sedang memasuki nomor $lol"
+echo
+sleep 2
+echo $kuning "mungkin membutuhkan waktu beberapa menit untuk mendata ke nomor $lol"
+sleep 20
+echo
+clear
+echo $cyan   "selamat pendataan berhasil"
+echo
+sleep 3
+echo $kuning "sekarang anda dapat melihat aktivitas nomor $lol"
+sleep 3
+echo
+echo -n $cyan "tekan enter jika ingin melanjutkan ...........";read uy
+sleep 3
+clear
+echo $red "******************************************************************"
+echo
+echo $green  "$lol : HAI"
+echo
+echo $kuning "$iuy  : HAI JUGA "
+echo
+echo $green  "$lol : acara besok gimana.jadi?"
+echo
+echo $kuning "$iuy : ouh ya jelas jadi lah"
+echo
+echo $kuning "$iuy : btw. kamu lagi ngapain >_<"
+echo
+echo $green  "$lol : biasa lagi duduk-duduk di teras rumah"
+echo
+echo $kuning "$iuy : ouh lagi duduk-duduk aja"
+echo
+echo $kuning "$iuy : kalau kamu lagi ngapain ?"
+echo
+echo $green  "$lol : aku sih lagi chatan ama kamu"
+echo
+echo $green  "$lol : sama nonton youtube sih"
+echo
+echo $kuning "$iuy : nonton apa hayo di youtube >_<"
+echo
+echo $kuning "$iuy : jangan jangan nonton >_<"
+echo
+echo $green  "$lol : gak lagi nonton di channel yt CYTOM"
+echo
+echo $green  "$lol : sama nonton qaf media channel"
+echo
+echo $kuning "$iuy : emang chanel apaan itu yah"
+echo
+echo $green  "$lol : penasaran kan nonton aja aku kasih link"
+echo
+echo $kuning "$iuy : ouh cepet kasih link nya"
+echo
+echo $green  "$lol : tonton aja dulu kalo suka boleh subrek"
+echo
+echo $green  "$lol : soal nya itu yt punya temen"
+echo
+echo $kuning "$iuy : iya "
+echo
+echo $green  "$lol : siiip"
+echo
+echo $kuning "$iuy : mana cepet"
+echo
+echo $green  "$lol : ini link CYTOM "
+echo
+echo $green  "$lol : https://youtu.be/-iZ-LKwM7yY"
+echo
+echo $green  "$lol : ini link qaf media channel"
+echo
+echo $green  "$lol : https://youtu.be/S3bA6eYFCrE  "
+echo
+echo $green  "$lol : kalo qaf media chanel itu bukan punya si CYTOM"
+echo
+echo $green  "$lol : dia suka nonton jadi gua ikutan dah"
+echo
+echo $kuning "$iuy  : yah udah saya tonton yah"
+echo
+echo $green  "$lol : siip"
+echo
+echo $red "******************************************************************"
+echo
+echo "TEKAN CTRL + C UNTUK KELUAR PROGRAM"
+sleep 500000000000
+clear
+
+
+
+
+
+
+
+
+
+
+
+
+
